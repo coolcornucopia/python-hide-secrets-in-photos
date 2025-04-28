1370920c1dcb8882c3069fb4d0f8c9cbaa8e8d9a
coolcornucopia <coolcornucopia@outlook.com>
2025-04-28 12:23:39 +0200

    xor180: add more comments in decode/encode scripts
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- xor180/decode.py
+++ xor180/decode.py
@@ -1,31 +1,31 @@
-# Usage : python3 decode.py file_to_decode.jpg decoded_file.png
-
-# XOR an image with its 180 rotated version, here below B is simple the 180-rotation of A
-# A xor B = C
-# C xor B = A
-# A xor C = B
-# C xor A = B
-
 import cv2
-import numpy as np
 import sys
 
-#print(sys.argv, len(sys.argv))
+# Check parameters
 if len(sys.argv) != 3:
-    print("Usage: python3 decode.py file_to_decode.jpg decoded_file.png")
+    print(sys.argv[0], ": Decode a XOR+180 encoded photo")
+    print("Usual image formats are supported (jpg, png, gif...).")
+    print("Usage: python3", sys.argv[0], "file_to_decode.jpg decoded_file.png")
     exit(1)
 
+# Get parameter filenames
 img_input_filename = sys.argv[1]
 img_output_filename = sys.argv[2]
 
+# Read input photo
 img_input = cv2.imread(img_input_filename)
 #print(img_input.shape)
+
+# Duplicate input photo to a 180 version
 img_input_rot180 = cv2.rotate(img_input, cv2.ROTATE_180)
 
-# case A xor B = C
+# Perform the xor operation between the 2 photos
 img_output_xor = cv2.bitwise_xor(img_input, img_input_rot180, mask = None)
+
+# Save the result in the output photo file
 cv2.imwrite(img_output_filename, img_output_xor)
 
+# Display results
 cv2.imshow("img_input", img_input)
 cv2.imshow("img_output_xor", img_output_xor)
 
--- xor180/encode.py
+++ xor180/encode.py
@@ -1,40 +1,41 @@
-# Usage : python3 encode.py file_to_encode.jpg encoded_file.png
-
-# XOR an image with its 180 rotated version, here below B is simple the 180-rotation of A
-# A xor B = C
-# C xor B = A
-# A xor C = B
-# C xor A = B
-
 import cv2
 import numpy as np
 import sys
 
-#print(sys.argv, len(sys.argv))
+# Check parameters
 if len(sys.argv) != 3:
-    print("Usage: python3 encode.py file_to_encode.jpg encoded_file.png")
+    print(sys.argv[0], ": Encode a photo with XOR+180+RANDOM operations")
+    print("Usual image formats are supported (jpg, png, gif...).")
+    print("Usage: python3", sys.argv[0], "file_to_encode.jpg encoded_file.png")
     exit(1)
 
+# Get parameter filenames
 img_input_filename = sys.argv[1]
 img_output_filename = sys.argv[2]
 
+# Read input photo
 img_input = cv2.imread(img_input_filename)
-print(img_input.shape)
+#print(img_input.shape)
 
 # Generate a random noise
 r = 255 * np.random.rand(*img_input.shape)
 img_random = r.astype(np.uint8)
-print(img_random.shape)
-img_random_rot180 = np.flip(np.flip(img_random, 1), 0) # 2 symmetries along each direction = rot180
-print(img_random_rot180.shape)
+#print(img_random.shape)
 
-# case A xor B = C
+# Duplicate the random noise photo with a 180° rotation
+img_random_rot180 = np.flip(np.flip(img_random, 1), 0) # 2 symmetries along each direction = rot180
+#print(img_random_rot180.shape)
+
+# Perform the xor operation between the input photo and the random noise
 img_output = cv2.bitwise_xor(img_input, img_random, mask = None)
 
+# Concatenate the above xor result photo with the 180° random noise photo
 img_output = np.concatenate((img_output, img_random_rot180))
 
+# Save the result in the output photo file
 cv2.imwrite(img_output_filename, img_output)
 
+# Display results
 cv2.imshow("img_input", img_input)
 cv2.imshow("img_output", img_output)
 
